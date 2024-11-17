5ec6e9846feff4d06e22350e94657180fedc0683
Joao Victor Pietchaki Gonçalves <victorpietchaki@ufpr.br>
2024-11-17 00:01:18 -0300

    Conferindo funcionamento do template no Rmarkdown.

--- 02_scripts/024_Rmarkdown/render-report.R
+++ 02_scripts/024_Rmarkdown/render-report.R
@@ -1,8 +1,15 @@
-# Sincroniza
+# Ativa ambiente virtual e sincroniza pacotes instalados/removidos
+renv::activate()
 renv::restore()
 
 require(rmarkdown)
 require(here)
+require(remotes)
+require(tinytex)
+require(xtable)
+
+tinytex::install_tinytex()
+tinytex::tlmgr_install("babel-portuges")
 
 # Define os caminhos do arquivo de entrada e do renderizado e atribuia a variaveis
 input_path <- here("02_scripts/024_Rmarkdown/Data-Analysis-Report", "Data-Analysis-Report.Rmd")
--- 02_scripts/024_Rmarkdown/set-template.R
+++ 02_scripts/024_Rmarkdown/set-template.R
@@ -1,4 +1,8 @@
 # Instala template de arquivos .pdf do Professor Fernando Mayer (UFPR)
 # Possibilita criar arquivos .Rmd no template desejado
+
 require(remotes)
-install_github("fernandomayer/tcctemplate")
+require(tinytex)
+remotes::install_github("fernandomayer/tcctemplate")
+tinytex::install_tinytex()
+tinytex::tlmgr_install("babel-portuges")
